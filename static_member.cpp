1dc79c7d6ad3837b7c1567dc04b77941633c7905
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-01-03 10:44:10 +0530

    Static Member

---
+++ static_member.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class MyClass{
+public:
+    static int staticVariable; // declaration of static variable
+
+    static void staticMethod(){
+        cout<<"Static Method defination"<<endl;
+    }
+};
+
+int MyClass::staticVariable = 0 ; //Initialisation of static variable
+
+int main(){
+    MyClass::staticVariable = 10;//accessing static variable
+    MyClass::staticMethod(); //accessing static method
+    cout<<MyClass::staticVariable;
+    return 0;
+}
